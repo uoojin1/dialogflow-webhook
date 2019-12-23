4ffc8e01d66888a32a2bb3b336bc4a22035d516e
Eugene Moon <uoojin95@gmail.com>
2019-12-23 11:10:06 +0900

    ✨ Feat: adding morgan as the server logger

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = process.env.PORT || 5000
+
+// add logging middleware
+app.use(morgan())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/fulfillment', function(req, res) {
+  console.log("request body\n\n", req.body)
   res.sendStatus(200)
 })
 
